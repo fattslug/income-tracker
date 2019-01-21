36c42a46679ca8e32d4c2cf879494ff52a232378
Sean Powell <ssppowell@gmail.com>
2019-01-20 21:48:09 -0500

    Adding errors to form

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import { Box, Button, FormField, MaskedInput, Select, Text, TextInput } from 'grommet';
 import axios from 'axios';
 
+function Error(props) {
+  if (props.show) {
+    return (
+      <Box
+        background='status-error'
+        color='light-1'
+        round='small'
+        pad='small'
+      >
+        {props.children}
+      </Box>
+    );
+  } else {
+    return null;
+  }
+}
+
 class EntryForm extends Component {
   constructor() {
     super();
@@ -40,19 +57,54 @@ class EntryForm extends Component {
           name: 'Treatment'
         }, {
           name: 'Gloss/Toner'
-        }]
-      }
+        }],
+        paymentOptions: ['Cash', 'Credit Card', 'Venmo', 'Apple Pay']
+      },
+      errors: {}
     }
   }
 
   parseCurrency = (value) => {
-    return parseFloat(value.substr(1, value.length));
+    return parseFloat(value.substr(1, value.length)) || 0;
   }
 
-  handleChange = (propertyName, newValue) => {
-    let currentState = this.state.values;
-    currentState[propertyName] = newValue;
-    this.setState(currentState);
+  handleChange = (fieldName, newValue) => {
+    let currentValues = this.state.values;
+    let currentErrors = this.state.errors;
+    
+    currentValues[fieldName] = newValue;
+    currentErrors[fieldName] = this.validateField(fieldName);
+    console.log(this.validateField(fieldName));
+
+    this.setState({
+      values: currentValues,
+      errors: currentErrors
+    });
+  }
+
+  validateField = (fieldName) => {
+    if (fieldName === 'AmountPaidString') {
+      return (this.parseCurrency(this.state.values[fieldName]) <= 0);
+    } else if (fieldName === 'ClientName') {
+      return (this.state.values[fieldName] === '');
+    } else if (fieldName === 'ServicesRendered') {
+      return (this.state.values[fieldName] <= 0);
+    } else if (fieldName === 'PaymentType') {
+      return (this.state.values[fieldName] === '');
+    }
+  }
+
+  validateForm = () => {
+    return new Promise((resolve) => {
+      const errors = {
+        AmountPaidString: this.validateField('AmountPaidString'),
+        ClientName: this.validateField('ClientName'),
+        ServicesRendered: this.validateField('ServicesRendered'),
+        PaymentType: this.validateField('PaymentType')
+      };
+      this.setState({ errors: errors });
+      resolve(Object.values(errors).find(err => err === true) || false);
+    });
   }
 
   handleSubmit = (e) => {
@@ -62,17 +114,21 @@ class EntryForm extends Component {
     let currentState = this.state.values;
     currentState.AmountPaid = this.parseCurrency(this.state.values.AmountPaidString);
     this.setState(currentState);
-    
-    // Submit data
-    axios.post(process.env.REACT_APP_SERVICE_URL + '/entries/', { entry: this.state.values }).then((result) => {
-      console.log(result);
+
+    this.validateForm().then((hasErrors) => {
+      if (!hasErrors) {
+        // Submit data
+        axios.post(process.env.REACT_APP_SERVICE_URL + '/entries/', { entry: this.state.values }).then((result) => {
+          console.log('Result', result);
+        });
+      }
     });
   }
 
   render() {
     const { ClientName, PaymentType, ServicesRendered, AmountPaidString } = this.state.values;
-    const { serviceOptions } = this.state.options;
-    const paymentOptions = ['Cash', 'Credit Card', 'Venmo', 'Apple Pay'];
+    const { serviceOptions, paymentOptions } = this.state.options;
+    const errors = this.state.errors;
     return (
       <form onSubmit={this.handleSubmit}>
         <Box fill align="center" justify="start" pad="large">
@@ -89,6 +145,7 @@ class EntryForm extends Component {
                   onChange={(e) => this.handleChange('ClientName', e.target.value)}
                 />
               </FormField>
+              <Error show={errors.ClientName}>Please enter a client name.</Error>
             </Box>
             <Box pad="medium">
               <FormField label="Payment Type" htmlFor="text-input">
@@ -101,6 +158,7 @@ class EntryForm extends Component {
                   onChange={({ option }) => this.handleChange('PaymentType', option)}
                 />
               </FormField>
+              <Error show={errors.PaymentType}>Please select a payment type.</Error>
             </Box>
             <Box pad="medium">
               <FormField label="Services" htmlFor="text-input">
@@ -124,6 +182,7 @@ class EntryForm extends Component {
                   }}
                 />
               </FormField>
+              <Error show={errors.ServicesRendered}>Please select services rendered for this client.</Error>
             </Box>
             <Box pad="medium">
               <FormField label="Amount Paid" htmlFor="text-input">
@@ -145,6 +204,7 @@ class EntryForm extends Component {
                   onChange={(e) => this.handleChange('AmountPaidString', e.target.value)}
                 />
               </FormField>
+              <Error show={errors.AmountPaidString}>Please enter the amount paid by this client.</Error>
             </Box>
             <Box pad="medium">
               <Button label="Submit" type='submit' primary={true} onClick={(e) => { this.handleSubmit(e) }} />
